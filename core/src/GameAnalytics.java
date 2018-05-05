e4cc63ae320417081b720b4ab66dc5e4d873b5ae
Benjamin Schulte <bs@golfgl.de>
2018-05-05 13:10:13 +0200

    Gameanalytics example adjusted to libgdx

---
+++ core/src/GameAnalytics.java
@@ -0,0 +1,443 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net;
+import com.badlogic.gdx.utils.Base64Coder;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.utils.JsonWriter;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Gameanalytics.com client for libGDX
+ * <p>
+ * Created by Benjamin Schulte on 05.05.2018 based up on example implementation
+ * https://s3.amazonaws.com/download.gameanalytics.com/examples/GameAnalytics+REST+API+example.java
+ */
+
+public class GameAnalytics {
+    private static final String TAG = "Gameanalytics";
+    private final static String sdk_version = "rest api v2";
+    //sandbox game keys. Change with your game's keys when ready
+    private String game_key = "5c6bcb5402204249437fb5a7a80a4959";
+    private String secret_key = "16813a12f718bc5c620f56944e1abc3ea13ccbac";
+    //sandbox API URLs. Change with live end-point when ready http://api.gameanalytics.com/v2/
+    private String url = "http://sandbox-api.gameanalytics.com/v2/";
+    //device information
+    private String platform = "libgdx";
+    private String os_version = "unknown";
+    private String device = "unknown";
+    private String manufacturer = "unkown";
+
+    //game information
+    private String build;
+
+    //user information
+    //FIXME load and calc
+    private String user_id = "AEBE52E7-03EE-455A-B3C4-E57283966239"; //example
+    private String session_id;
+    //FIXME load and calc
+    private int session_num = 5;
+
+    //event category
+    private EventCategory category;
+
+    //SDK status
+    private boolean canSendEvents = false;
+    private int lastFailedWait = 0;
+
+    //TODO maximum queue size
+    private List<AnnotatedEvent> queue = new ArrayList<AnnotatedEvent>();
+
+    private static String generateHash(String json, String secretKey) {
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            byte[] encoded = secretKey.getBytes();
+            SecretKeySpec secretKeySpec = new SecretKeySpec(encoded, "HmacSHA256");
+            sha256_HMAC.init(secretKeySpec);
+            return new String(Base64Coder.encode(sha256_HMAC.doFinal(json.getBytes())));
+        } catch (Exception ex) {
+            System.out.println("Error generating Hmac: " + ex.toString());
+            return "";
+        }
+    }
+
+    protected void flushQueue() {
+        if (!canSendEvents)
+            return;
+
+        if (lastFailedWait > 0) {
+            lastFailedWait -= 1;
+            return;
+        }
+
+        String payload = "[";
+        Json json = new Json();
+        json.setOutputType(JsonWriter.OutputType.json);
+
+        for (int i = 0; i < queue.size(); i++) {
+            if (i != queue.size() - 1) {
+                payload += json.toJson(queue.get(i)) + ",\n";
+            } else {
+                payload += json.toJson(queue.get(i)) + "\n";
+            }
+        }
+        payload += "]";
+
+        final Net.HttpRequest request = new Net.HttpRequest("POST");
+        request.setUrl(url + game_key + "/events");
+        request.setContent(payload);
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+
+        // authorization
+        String hash = generateHash(payload, secret_key);
+        request.setHeader("Authorization", hash);
+
+        //FIXME make sure data is not send twice (boolean flag)
+
+        //Execute and read response
+        Gdx.net.sendHttpRequest(request, new Net.HttpResponseListener() {
+            @Override
+            public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                queue.clear();
+                //FIXME process other status codes
+                Gdx.app.debug(TAG, request.getContent() + "\n" + httpResponse.getResultAsString());
+            }
+
+            @Override
+            public void failed(Throwable t) {
+                failed();
+            }
+
+            @Override
+            public void cancelled() {
+                failed();
+            }
+
+            private void failed() {
+                Gdx.app.error(TAG, "could not send events in queue.");
+                addLastFailed();
+            }
+        });
+    }
+
+    private void addLastFailed() {
+        lastFailedWait = Math.max(lastFailedWait * 2, 2);
+        lastFailedWait = Math.min(15, lastFailedWait);
+    }
+
+    private String generateSessionID() {
+        UUID sid = UUID.randomUUID();
+        return sid.toString();
+    }
+
+    private void createUserEvent() {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.user;
+        event.put("category", category.toString());
+        queue.add(event);
+    }
+
+    public void submitDesignEvent(String event_id) {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.design;
+        event.put("category", category.toString());
+        event.put("event_id", event_id);
+        queue.add(event);
+    }
+
+    public void submitDesignEvent(String event_id, float amount) {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.design;
+        event.put("category", category.toString());
+        event.put("event_id", event_id);
+        event.putFloat("amount", amount);
+        queue.add(event);
+    }
+
+    private void createGenericBusinessEvent(String event_id, int amount, String currency, int transaction_num) {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.business;
+        event.put("category", category.toString());
+        event.put("event_id", event_id);
+        event.putInt("amount", amount);
+        event.put("currency", currency);
+        event.putInt("transaction_num", transaction_num);
+        queue.add(event);
+    }
+
+    private void createGenericBusinessEventGoogle(String event_id, int amount, String currency, int
+            transaction_num, String cart_type, String receipt, String signature) {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.business;
+        event.put("category", category.toString());
+        event.put("event_id", event_id);
+        event.putInt("amount", amount);
+        event.put("currency", currency);
+        event.putInt("transaction_num", transaction_num);
+        event.put("cart_type", cart_type);
+
+        // TODO
+        //receipt
+        //JSONObject receipt_info = new JSONObject();
+        //receipt_info.put("receipt", receipt);
+        //receipt_info.put("signature", signature);
+        //event.put("receipt_info", receipt_info);
+        queue.add(event);
+    }
+
+    public void submitProgressionEvent(ProgressionStatus status, String progression01, String progression02,
+                                       String progression03) {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.progression;
+        event.put("category", category.toString());
+
+        String event_id = status.toString() + ":" + progression01;
+        if (progression02.length() > 0) {
+            event_id += ":" + progression02;
+        }
+        if (progression03.length() > 0) {
+            event_id += ":" + progression03;
+        }
+        event.put("event_id", event_id);
+
+        if (status == ProgressionStatus.Complete || status == ProgressionStatus.Fail) {
+            int attempt_num = 1; //increment each time this particular progression event has been generated with
+            // status fail.
+            event.putInt("attempt_num", attempt_num);
+        }
+        queue.add(event);
+    }
+
+    public void submitProgressionEvent(ProgressionStatus status, String progression01, String progression02,
+                                       String progression03, int score) {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.progression;
+        event.put("category", category.toString());
+
+        String event_id = status.toString() + ":" + progression01;
+        if (progression02.length() > 0) {
+            event_id += ":" + progression02;
+        }
+        if (progression03.length() > 0) {
+            event_id += ":" + progression03;
+        }
+        event.put("event_id", event_id);
+
+        if (status == ProgressionStatus.Complete || status == ProgressionStatus.Fail) {
+            int attempt_num = 1; //increment each time this particular progression event has been generated with
+            // status fail.
+            event.putInt("attempt_num", attempt_num);
+            event.putInt("score", score);
+        }
+        queue.add(event);
+    }
+
+    private void createResourceEvent(ResourceFlowType flowType, String virtualCurrency, String itemType,
+                                     String itemId, float amount) {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.resource;
+        event.put("category", category.toString());
+
+        String event_id = flowType.toString() + ":" + virtualCurrency + ":" + itemType + ":" + itemId;
+        event.put("event_id", event_id);
+        event.putFloat("amount", amount);
+        queue.add(event);
+    }
+
+    private void createErrorEvent(ErrorType severity, String message) {
+        AnnotatedEvent event = new AnnotatedEvent();
+        category = EventCategory.error;
+        event.put("category", category.toString());
+        event.put("severity", severity.toString());
+        event.put("message", message);
+        queue.add(event);
+    }
+
+    protected void closeSession() {
+        AnnotatedEvent session_end_event = new AnnotatedEvent();
+        category = EventCategory.session_end;
+        session_end_event.put("category", category.toString());
+        session_end_event.putInt("length", 60); // record locally how much time the session took and send it here. 60 is
+        // an example
+        sendEvent(session_end_event);
+
+    }
+
+    /**
+     * send init request
+     */
+    protected void createInitRequest() {
+        Json json = new Json();
+        json.setOutputType(JsonWriter.OutputType.json);
+
+        String event = "[" + json.toJson(new InitEvent()) + "]";
+
+        final Net.HttpRequest request = new Net.HttpRequest("POST");
+        request.setUrl(url + game_key + "/init");
+        request.setContent(event);
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+
+        // authorization
+        String hash = generateHash(event, secret_key);
+        request.setHeader("Authorization", hash);
+
+        //Execute and read response
+        Gdx.net.sendHttpRequest(request, new Net.HttpResponseListener() {
+            @Override
+            public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                canSendEvents = httpResponse.getStatus().getStatusCode() == 200;
+                Gdx.app.debug(TAG, request.getContent() + "\n" + httpResponse.getResultAsString());
+            }
+
+            @Override
+            public void failed(Throwable t) {
+                canSendEvents = false;
+            }
+
+            @Override
+            public void cancelled() {
+                canSendEvents = false;
+            }
+        });
+    }
+
+    private void sendEvent(AnnotatedEvent eventJson) {
+        Json json = new Json();
+        json.setOutputType(JsonWriter.OutputType.json);
+
+        String event = "[" + json.toJson(eventJson) + "]";
+
+        final Net.HttpRequest request = new Net.HttpRequest("POST");
+        request.setUrl(url + game_key + "/events");
+        request.setContent(event);
+        request.setHeader("Accept", "application/json");
+        request.setHeader("Content-type", "application/json");
+
+        // authorization
+        String hash = generateHash(event, secret_key);
+        request.setHeader("Authorization", hash);
+
+        //Execute and read response
+        Gdx.net.sendHttpRequest(request, new Net.HttpResponseListener() {
+            @Override
+            public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                Gdx.app.debug(TAG, request.getContent() + "\n" + httpResponse.getResultAsString());
+            }
+
+            @Override
+            public void failed(Throwable t) {
+                failed();
+            }
+
+            @Override
+            public void cancelled() {
+                failed();
+            }
+
+            private void failed() {
+                addLastFailed();
+            }
+        });
+    }
+
+    /**
+     * @return if events are sent to gameanalytics after a successful login
+     */
+    public boolean canSend() {
+        return canSendEvents;
+    }
+
+    public void initSession() {
+        session_id = generateSessionID();
+        createInitRequest();
+        createUserEvent();
+    }
+
+    public enum ProgressionStatus {Start, Fail, Complete}
+
+    public enum EventCategory {
+        user, design, business, progression, resource, error, session_end
+    }
+
+    public enum ResourceFlowType {Sink, Source}
+
+    ;
+
+    public enum ErrorType {debug, info, warning, error, critical}
+
+    ;
+
+    private class InitEvent implements Json.Serializable {
+        @Override
+        public void write(Json json) {
+            json.writeValue("platform", platform);
+            json.writeValue(os_version, os_version);
+            json.writeValue("sdk_version", sdk_version);
+        }
+
+        @Override
+        public void read(Json json, JsonValue jsonData) {
+            // not implemented
+        }
+    }
+
+    ;
+
+    private class AnnotatedEvent implements Json.Serializable {
+        private Map<String, Object> keyValues = new HashMap<>();
+
+        public AnnotatedEvent() {
+            //this is stored
+            keyValues.put("client_ts", (Long) TimeUtils.millis() / 1000L);
+        }
+
+        @Override
+        public void write(Json event) {
+            event.writeValue("platform", platform);
+            event.writeValue("os_version", os_version);
+            event.writeValue("sdk_version", sdk_version);
+            event.writeValue("device", device);
+            event.writeValue("manufacturer", manufacturer);
+            if (build != null)
+                event.writeValue("build", build);
+            event.writeValue("user_id", user_id);
+            event.writeValue("session_id", session_id);
+            event.writeValue("session_num", session_num);
+            event.writeValue("v", 2);
+
+            for (String key : keyValues.keySet()) {
+                event.writeValue(key, keyValues.get(key));
+            }
+        }
+
+        @Override
+        public void read(Json json, JsonValue jsonData) {
+            // not supported
+        }
+
+        public void put(String name, String value) {
+            keyValues.put(name, value);
+        }
+
+        public void putInt(String name, int value) {
+            keyValues.put(name, value);
+        }
+
+        public void putFloat(String name, float value) {
+            keyValues.put(name, value);
+        }
+    }
+
+
+
+}
---
+++ core/test/GameAnalyticsTest.java
@@ -0,0 +1,90 @@
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.graphics.GL20;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * Created by Benjamin Schulte on 05.05.2018.
+ */
+public class GameAnalyticsTest {
+
+    @BeforeClass
+    public static void init() {
+        // Note that we don't need to implement any of the listener's methods
+        Gdx.app = new HeadlessApplication(new ApplicationListener() {
+            @Override
+            public void create() {
+            }
+
+            @Override
+            public void resize(int width, int height) {
+            }
+
+            @Override
+            public void render() {
+            }
+
+            @Override
+            public void pause() {
+            }
+
+            @Override
+            public void resume() {
+            }
+
+            @Override
+            public void dispose() {
+            }
+        });
+
+        // Use Mockito to mock the OpenGL methods since we are running headlessly
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+    }
+
+    @Test
+    public void testGameAnalytics() throws InterruptedException {
+        GameAnalytics ga = new GameAnalytics();
+
+        ga.initSession();
+
+        Thread.sleep(500); // give the HTTP request some time
+
+        //Design events
+        ga.submitDesignEvent("kill:robot:blue");
+        ga.submitDesignEvent("Tutorial:Step1:Finished", 100f);
+
+        //Business event
+        //CreateGenericBusinessEvent("Sales:GoodiesPackage", 99, "USD", 1);
+        //Business event for Google
+        //CreateGenericBusinessEventGoogle("Sales", 99, "USD", 1, "Tier 1", "+Receipt+", "+signature+");
+
+        //Progression events
+        ga.submitProgressionEvent(GameAnalytics.ProgressionStatus.Start, "World1", "", "");
+        ga.submitProgressionEvent(GameAnalytics.ProgressionStatus.Fail, "World1", "Level2", "");
+        ga.submitProgressionEvent(GameAnalytics.ProgressionStatus.Complete, "World2", "Level1", "Arena2", 200);
+
+        //Resource events
+        //CreateResourceEvent(GameAnalytics.ResourceFlowType.Sink, "gold", "Weapon", "Frostmourne", 10000);
+        //CreateResourceEvent(GameAnalytics.ResourceFlowType.Source, "silver", "Consumable", "Mana potion", 5);
+
+        //Error event
+        //CreateErrorEvent(GameAnalytics.ErrorType.info, "The instrumentation runs smooth!");
+
+        //Send all events in queue
+        ga.flushQueue();
+
+        //Finally close session by sending session_end event
+        ga.closeSession();
+
+        Thread.sleep(1500); // give the HTTP request some time
+
+    }
+}
